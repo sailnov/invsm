71ffa0ebb0f81cc4b73ac9be72112e9534db4eda
pemag10 <mkouki1959@gmail.com>
2022-05-20 18:57:31 +0900

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,4 +70,11 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
+    router: {
+        base: '/invsm/'
+      },
+    
+      generate: {
+        dir: 'docs'
+      },
 };
